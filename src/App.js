a21cb23409d8df84547971158fd4608fe1522854
r_allada <rallada@credenti.io>
2022-06-04 17:41:55 +0530

    Integrated users API

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import './App.css';
 import EditableTable from './components/Table';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const data = [{name: 'Ram', email: 'ram@gmail.com'}, {name:'Krishna', email: 'krishna@gmail.com'}];
+	const [users, setUsers] = useState();
 
-  return (
-    <div className="App">
-      <div className='root-container'>
-        <input placeholder='Search' />
-          <EditableTable data={data} />
-      </div>
-    </div>
-  );
+	useEffect(() => {
+		fetch('https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json')
+			.then(res => res.json())
+			.then(data => setUsers(data));
+	}, []);
+
+
+	return (
+		<div className="App">
+			<div className='root-container'>
+				<input placeholder='Search' />
+				<EditableTable data={users} />
+			</div>
+		</div>
+	);
 }
 
 export default App;
