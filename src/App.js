b523b2e4268362d0a511d16ddaaea8fb2497c4fb
r_allada <rallada@credenti.io>
2022-06-05 13:03:36 +0530

    Search bar functionality added

--- src/App.js
+++ src/App.js
@@ -5,22 +5,38 @@ import React, { useState, useEffect } from 'react';
 function App() {
 	const [users, setUsers] = useState();
 	const [columns, setColumns] = useState([]);
-	
+	const [data, setData] = useState();
+
 	useEffect(() => {
 		fetch('https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json')
 			.then(res => res.json())
-			.then(data => setUsers(data));
+			.then(data => {
+				setData(data);
+				setUsers(data);
+			});
 	}, []);
 
 	useEffect(() => {
-	  setColumns(Object.keys(users?.[0] ? users?.[0] : {}));
-	}, [users]);
+		setColumns(Object.keys(data?.[0] ? data?.[0] : {}));
+	}, [data]);
+
+	function handleFilterChange(e) {
+		const queryStr = e.target.value;
+		console.log(queryStr);
+		setUsers(
+			data.filter(user =>
+				user.name.includes(queryStr) || user.email.includes(queryStr) || user.role.includes(queryStr) || !queryStr
+			)
+		);
+	}
 
 	return (
 		<div className="App">
 			<div className='root-container'>
-				<input placeholder='Search' />
-				<EditableTable data={users} columns={columns}/>
+				<input placeholder='Search'
+					onChange={handleFilterChange}
+				/>
+				<EditableTable data={users} columns={columns} />
 			</div>
 		</div>
 	);
